8d48cdcf803d62b7269d177dbf853d29fab120b3
Umit8098 <81574109+Umit8098@users.noreply.github.com>
2022-02-19 00:58:09 +0300

    commit

--- lesson-11 Django - Auth - 1. session/user_example/views.py
+++ lesson-11 Django - Auth - 1. session/user_example/views.py
@@ -1,4 +1,3 @@
-import re
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
